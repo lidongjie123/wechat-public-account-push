f3047bc7ccce748b980b8343b273b9e2f0e49f56
lidongjie123 <784983957@qq.com>
2023-05-15 22:16:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oLfKt6dva49Zm_-Tw87_k-O_8qFo',
+      id: 'oLfKt6dNaV0PejIxJN_8g_9DcyEc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'zjDmO5LvmVpt7JdqyCy1lZdwclvaKjEu7amceNa2W0o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
